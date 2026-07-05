32e09a09728b7bcb5c4738053976759bb9ec93b0
AndrePassoni <andre.magalhaes.passoni@gmail.com>
2026-07-05 14:04:52 -0300

    feat: implement disk storage for file handling and enhance upload functionality

--- src/controllers/uploads-controller.ts
+++ src/controllers/uploads-controller.ts
@@ -1,10 +1,14 @@
 import { Request, Response } from "express"
-import { z } from "zod"
+import { z, ZodError } from "zod"
 
 import uploadConfig from "@/configs/upload"
+import { DiskStorage } from "@/providers/disk-storage"
+import { AppError } from "@/utils/AppError"
 
 export class UploadsController {
     async create(request: Request, response: Response) {
+        const diskStorage = new DiskStorage()
+
         try {
             const fileSchema = z.object({
                 filename: z.string().min(1, "Arquivo é obrigatório"),
@@ -13,10 +17,19 @@ export class UploadsController {
                 `Arquivo excede o tamanho máximo de ${uploadConfig.MAX_SIZE}`),
             }).passthrough()
 
-            const { file } = fileSchema.parse(request.file)
+            const file = fileSchema.parse(request.file)
+            const filename = await diskStorage.saveFile(file.filename)
 
-            response.json({ message: "ok" })
+            response.json({ filename })
         } catch (error) {
+            if (error instanceof ZodError) {
+                if (request.file) {
+                    await diskStorage.deleteFile(request.file.filename, "tmp")
+                }
+
+                throw new AppError(error.issues[0].message)
+            }
+
             throw error
         }
     }
